cd98e5e8337ab559caa0c884b3753864e18011f0
Fanny Petersen <petersen.fanny@gmail.com>
2025-05-27 18:42:14 +0200

    Use icon and pink background for add button

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,10 @@ function App() {
         onChange={e => setInput(e.target.value)}
         placeholder="Add a new todo"
       />
-      <button onClick={handleAddTodo}>Add</button>
+      <button onClick={handleAddTodo} aria-label="Add todo">
+        {/* Plus SVG icon */}
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="12" y1="5" x2="12" y2="19"/><line x1="5" y1="12" x2="19" y2="12"/></svg>
+      </button>
       <ul>
         {todos.map((todo, idx) => (
           <li key={idx}>
