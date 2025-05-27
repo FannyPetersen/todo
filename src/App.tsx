bc8803d80faf105a450dc2830cfb5ee3af038304
Fanny Petersen <petersen.fanny@gmail.com>
2025-05-27 17:37:09 +0200

    Remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,25 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  // State to store the list of todos (array of strings)
   const [todos, setTodos] = useState<string[]>([])
-  // State to store the current value of the input field
   const [input, setInput] = useState<string>("")
-
-  // Function to handle adding a new todo
+  
   const handleAddTodo = () => {
-    if (input.trim() === "") return // Prevent adding empty todos
-    setTodos([...todos, input]) // Add the new todo to the list
-    setInput("") // Clear the input field
+    if (input.trim() === "") return
+    setTodos([...todos, input]) 
+    setInput("") 
   }
 
   return (
     <div className="app-container">
       <h1>What to do</h1>
-      {/* Input field for new todos */}
       <input
         type="text"
         value={input}
         onChange={e => setInput(e.target.value)}
         placeholder="Add a new todo"
       />
-      {/* Button to add the todo */}
       <button onClick={handleAddTodo}>Add</button>
-      {/* List of todos */}
       <ul>
         {todos.map((todo, idx) => (
           <li key={idx}>{todo}</li>
